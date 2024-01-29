1a767806dd12c7f03be374d4e6aff4a02a7c3aff
aofpichetchai96 <pichetchai.s39@gmail.com>
2024-01-29 15:37:12 +0700

    update all File Project

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-let num = '1'
-console.log(num)
-console.log('test')
+const $ = (selector) => {
+    return document.querySelector(selector);
+}
+
+const hour = $('.hour');
+const dot = $('.dot');
+const min = $('.min');
+const week = $('.week');
+let showDot = true;
+
+function update(){
+  showDot = !showDot;
+  const now =  new Date();
+  
+  if(showDot){
+    dot.classList.add('invsible');
+  } else {
+    dot.classList.remove('invsible');
+  }
+  hour.textContent = String(now.getHours()).padStart(2, '0');
+  min.textContent = String(now.getMinutes()).padStart(2, '0');
+  
+  Array
+  .from(week.children)
+  .forEach(
+    (ele) => {
+        ele.classList.remove('active');
+    }
+  );
+  week.children[now.getDay()]
+  .classList
+  .add('active')
+
+};
+
+setinterval(update, 500)
