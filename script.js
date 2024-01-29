7aa9ffa81b15e02dee4a05e623bb90c5597a10fa
aofpichetchai96 <pichetchai.s39@gmail.com>
2024-01-29 16:34:03 +0700

    Update  all file

--- script.js
+++ script.js
@@ -2,35 +2,36 @@ const $ = (selector) => {
     return document.querySelector(selector);
 }
 
-const hour = $('.hour');
-const dot = $('.dot');
-const min = $('.min');
-const week = $('.week');
-let showDot = true;
-
+showDot = true;
 function update(){
-  showDot = !showDot;
-  const now =  new Date();
-  
-  if(showDot){
-    dot.classList.add('invsible');
-  } else {
-    dot.classList.remove('invsible');
-  }
-  hour.textContent = String(now.getHours()).padStart(2, '0');
-  min.textContent = String(now.getMinutes()).padStart(2, '0');
-  
-  Array
-  .from(week.children)
-  .forEach(
+    
+    const hour = document.getElementById('hour');
+    const dot = document.getElementById('dot');
+    const min = document.getElementById('min');
+    const week = document.getElementById('week');
+
+    showDot = !showDot;
+    const now =  new Date();
+
+    if(showDot){        
+        dot.classList.add("invsible");
+    } else {
+        dot.classList.remove('invsible');
+    }
+
+    hour.textContent = String(now.getHours());
+    min.textContent = String(now.getMinutes());
+
+    Array
+    .from(week.children)
+    .forEach(
     (ele) => {
         ele.classList.remove('active');
     }
-  );
-  week.children[now.getDay()]
-  .classList
-  .add('active')
-
+    );
+    week.children[now.getDay()]
+    .classList
+    .add('active')
 };
 
-setinterval(update, 500)
+setInterval(update, 500)
