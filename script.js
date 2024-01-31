feadfe4913c3d773b20463a42f99e1bf68f99980
aofpichetchai96 <pichetchai.s39@gmail.com>
2024-01-31 15:07:37 +0700

    Update add second

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ const $ = (selector) => {
 }
 
 showDot = true;
+
 function update(){
-    
     const hour = document.getElementById('hour');
     const dot = document.getElementById('dot');
+    const dot2 = document.getElementById('dot2');
     const min = document.getElementById('min');
+    const second = document.getElementById('second');
     const week = document.getElementById('week');
 
     showDot = !showDot;
     const now =  new Date();
-
+    
     if(showDot){        
         dot.classList.add("invsible");
+        dot2.classList.add("invsible");
     } else {
         dot.classList.remove('invsible');
+        dot2.classList.remove('invsible');
     }
 
     hour.textContent = String(now.getHours()).padStart(2, '0');
     min.textContent = String(now.getMinutes()).padStart(2, '0');
+    second.textContent = String(now.getSeconds()).padStart(2, '0');
+
 
     Array
     .from(week.children)
